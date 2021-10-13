dec19a87c0d9cf7f417fcdad6555b2d3fac0f855
Przemek <przemo247@outlook.com>
2021-10-13 22:57:11 +0200

    Increse icon size in SmallSizeMenu

--- src/components/SmallSizeMenu.tsx
+++ src/components/SmallSizeMenu.tsx
@@ -8,16 +8,16 @@ export const SmallSizeMenu = () => {
       <img className="logo logo--small" alt="" src={logo} />
       <ul className="smallsize-menu__list">
         <li className="smallsize-menu__item">
-          <BsPersonFill size="1.8rem" className="menu-icon" />
+          <BsPersonFill size="2rem" className="menu-icon" />
         </li>
         <li className="smallsize-menu__item">
-          <BsBarChartFill size="1.8rem" className="menu-icon" />
+          <BsBarChartFill size="2rem" className="menu-icon" />
         </li>
         <li className="smallsize-menu__item">
-          <GiAxeSword size="1.8rem" className="menu-icon" />
+          <GiAxeSword size="2rem" className="menu-icon" />
         </li>
         <li className="smallsize-menu__item">
-          <BsChatFill size="1.8rem" className="menu-icon" />
+          <BsChatFill size="2rem" className="menu-icon" />
         </li>
       </ul>
     </div>
