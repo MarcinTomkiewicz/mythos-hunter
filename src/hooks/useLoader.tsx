0c8ea664bc745d404e3244c6b9d1d45e70dc41b2
ossowskid <ossowski.dawid@gmail.com>
2021-10-29 20:12:24 +0200

    wip

--- src/hooks/useLoader.tsx
+++ src/hooks/useLoader.tsx
@@ -1,4 +1,4 @@
 export const useLoader = () => {
-    const loaderImage: string = "loader.gif";
-    return <img src={process.env.PUBLIC_URL + `/img/${loaderImage}`} alt="Loading" />
+    const loaderImage: string = "loader2.gif";
+    return <img src={process.env.PUBLIC_URL + `/img/${loaderImage}`} style={{ width: "40px",height: "40px"}} alt="Loading" />
 }
