256333f38984cb5b490823cc04ea35be96d8d797
Przemek <przemo247@outlook.com>
2021-11-15 23:29:16 +0100

    Change component names

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,22 @@ import "./scss/main.scss";
 import { FunctionComponent } from "react";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
-import Character from "./components/Character/Character";
-import Stats from "./components/Stats/Stats";
+import CharacterView from "./components/CharacterView/CharacterView";
+import Statistics from "./components/Statistics/Statistics";
 import Home from "./components/Home/Home";
 import NotFound from "./components/NotFound/NotFound";
 import Chat from "./components/Chat/Chat";
 import ChallengesOfGods from "./components/ChallengesOfGods/ChallengesOfGods";
+import Armoury from "./components/Armoury/Armoury";
 
 const App: FunctionComponent = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="character" element={<Character />} />
-        <Route path="stats" element={<Stats />} />
+        <Route path="armoury" element={<Armoury />} />
+        <Route path="character-view" element={<CharacterView />} />
+        <Route path="statistics" element={<Statistics />} />
         <Route path="challenges-of-gods" element={<ChallengesOfGods />} />
         <Route path="chat" element={<Chat />} />
         <Route path="*" element={<NotFound />} />
