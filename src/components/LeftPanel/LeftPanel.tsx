50f9efc32809dc40a782b3c86c9f9243af711111
Przemek <przemo247@outlook.com>
2021-11-15 21:58:16 +0100

    Change titles of boxes in the left panel

--- src/components/LeftPanel/LeftPanel.tsx
+++ src/components/LeftPanel/LeftPanel.tsx
@@ -4,12 +4,12 @@ import LeftPanelBox from "./LeftPanelBox";
 const LeftPanel: FunctionComponent = () => {
   return (
     <div className="left-panel">
-      <LeftPanelBox title="Dołącz do walki">
+      <LeftPanelBox title="Statystyki postaci">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat quas
         sit id doloribus. Nesciunt assumenda unde illo commodi reiciendis atque
         dicta obcaecati sapiente iure iste, amet officiis quibusdam ex totam.
       </LeftPanelBox>
-      <LeftPanelBox title="Gracze online">
+      <LeftPanelBox title="Surowce i inne">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit ipsa
         aliquid recusandae architecto, libero eius nulla soluta quo unde rem
         quia atque illo debitis aspernatur veniam nihil ipsum deserunt iste?
