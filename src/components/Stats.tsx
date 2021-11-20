5d629f63e7d1353905dd0abccdce3c912f078b7c
Przemek <przemo247@outlook.com>
2021-11-20 11:50:10 +0100

    Remove unnecessary components

--- src/components/Stats.tsx
+++
@@ -1,8 +0,0 @@
-export const Stats = () => {
-  return (
-    <div className="user-stats">
-      <div className="user-stats__points-left">Pozostało punktów:</div>
-      <form className="user-stats__form"></form>
-    </div>
-  );
-};
