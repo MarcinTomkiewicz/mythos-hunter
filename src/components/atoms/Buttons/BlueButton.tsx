998c80c0d4aea32dad7e9d6deeea0990213b3d0b
Przemek <przemo247@outlook.com>
2021-11-20 12:49:49 +0100

    Add Button Blue component

---
+++ src/components/atoms/Buttons/BlueButton.tsx
@@ -0,0 +1,41 @@
+import Button from "@mui/material/Button";
+import { FunctionComponent } from "react";
+
+interface ButtonBlueProps {
+  text: string;
+  onClick: () => void;
+  isDisabled: boolean;
+}
+
+const ButtonBlue: FunctionComponent<ButtonBlueProps> = ({
+  text,
+  onClick,
+  isDisabled = false,
+}) => {
+  if (!isDisabled) {
+    return (
+      <Button
+        variant="contained"
+        size="large"
+        style={{ fontSize: "12px", fontFamily: "inherit" }}
+        onClick={onClick}
+      >
+        {text}
+      </Button>
+    );
+  } else {
+    return (
+      <Button
+        variant="contained"
+        size="large"
+        className="btn-blue"
+        onClick={onClick}
+        disabled
+      >
+        {text}
+      </Button>
+    );
+  }
+};
+
+export default ButtonBlue;
