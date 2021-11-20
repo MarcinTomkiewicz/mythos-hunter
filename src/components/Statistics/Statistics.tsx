d0ead57082c44c329aa7822a42d5e20b3ffa6385
Przemek <przemo247@outlook.com>
2021-11-20 19:04:01 +0100

    Add statistics components

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -1,19 +1,63 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { db } from "../../config/firebaseConfig";
 import TopBar from "../atoms/TopBar/TopBar";
 import { useLanguagePacks } from "../../hooks/useLanguagePacks";
 import { useLanguageSettings } from "../../hooks/useLanguageSettings";
+import { useUser } from "../../hooks/useUser";
+import { StatisticsInterface } from "../../types/interfaces";
+import StatisticsItem from "./StatisticsItem";
+import ButtonBlue from "../atoms/Buttons/BlueButton";
 
 const Statistics: FunctionComponent = () => {
   const languagePacks = useLanguagePacks();
   const langCode = useLanguageSettings();
+  const [stats, setStats] = useState<StatisticsInterface[]>([]);
+  const [pointsLeft, setPointsLeft] = useState(10);
+  const user = useUser();
+  useEffect(() => {
+    const getStatistics = async () => {
+      try {
+        const docRef = doc(db, "users", user?.uid);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          setStats(docSnap.data().stats);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (user) {
+      getStatistics();
+    } else {
+      return;
+    }
+  }, [user]);
+  const updateDataInDb = async () => {
+    const docRef = doc(db, "users", user?.uid);
+    await updateDoc(docRef, {
+      stats,
+    });
+  };
   return (
     <>
       <TopBar title={languagePacks.headers?.statistics[langCode]} />
       <div className="content__wrapper">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore modi
-        fuga tenetur quidem voluptatem distinctio voluptas a libero eveniet enim
-        debitis excepturi perspiciatis rerum, aliquid obcaecati esse soluta
-        consequatur accusamus!
+        <div className="points-left">Pozostało punktów: {pointsLeft}</div>
+        {stats.map(({ abbr, points }) => (
+          <StatisticsItem
+            key={abbr}
+            abbr={abbr}
+            points={points}
+            pointsLeft={pointsLeft}
+            setStatistics={setStats}
+            setPointsLeft={setPointsLeft}
+          />
+        ))}
+        <ButtonBlue
+          text={languagePacks.labels?.apply[langCode]}
+          onClick={updateDataInDb}
+        />
       </div>
     </>
   );
---
+++ src/components/Statistics/StatisticsItem.tsx
@@ -0,0 +1,78 @@
+import React, { FunctionComponent } from "react";
+import { useLanguageSettings } from "../../hooks/useLanguageSettings";
+import { useLanguagePacks } from "../../hooks/useLanguagePacks";
+import { StatisticsInterface } from "../../types/interfaces";
+import Icon from "@mui/material/Icon";
+
+interface StatisticsItemProps {
+  abbr: string;
+  points: number;
+  pointsLeft: number;
+  setStatistics: React.Dispatch<React.SetStateAction<StatisticsInterface[]>>;
+  setPointsLeft: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const StatisticsItem: FunctionComponent<StatisticsItemProps> = ({
+  abbr,
+  points,
+  pointsLeft,
+  setStatistics,
+  setPointsLeft,
+}) => {
+  const languagePacks = useLanguagePacks();
+  const langCode = useLanguageSettings();
+
+  const handleIncreasePoints = () => {
+    if (pointsLeft === 0) {
+      return;
+    }
+    setStatistics((prevState) => {
+      return prevState.map((el) => {
+        if (abbr === el.abbr) {
+          return { ...el, points: el.points + 1 };
+        } else {
+          return el;
+        }
+      });
+    });
+    setPointsLeft((prevState) => prevState - 1);
+  };
+
+  const handleDecreasePoints = () => {
+    if (points === 1) {
+      return;
+    }
+    setStatistics((prevState) => {
+      return prevState.map((el) => {
+        if (abbr === el.abbr) {
+          return { ...el, points: el.points - 1 };
+        } else {
+          return el;
+        }
+      });
+    });
+    setPointsLeft((prevState) => prevState + 1);
+  };
+  return (
+    <div className="stat-item">
+      <span className="stat-item__name">
+        {languagePacks.statistics?.[abbr][langCode]}
+      </span>
+      <div className="stat-item__wrapper">
+        <div className="stat-item__points">{points}</div>
+        <button className="btn-round" onClick={handleIncreasePoints}>
+          <Icon color="primary" style={{ fontSize: "22px" }}>
+            add_circle
+          </Icon>
+        </button>
+        <button className="btn-round" onClick={handleDecreasePoints}>
+          <Icon color="error" style={{ fontSize: "22px" }}>
+            remove_circle
+          </Icon>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default StatisticsItem;
