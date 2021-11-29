d3f3837da6252bd7c5faedd903639e6e41e5de55
Przemek <przemo247@outlook.com>
2021-11-29 02:16:50 +0100

    Show different links for logged and not logged user

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,10 +2,26 @@ import { FunctionComponent } from "react";
 import { NavLink } from "react-router-dom";
 import { useLanguagePacks } from "../../hooks/useLanguagePacks";
 import { useLanguageSettings } from "../../hooks/useLanguageSettings";
+import { useUser } from "../../hooks/useUser";
 
 const Navigation: FunctionComponent = () => {
   const languagePacks = useLanguagePacks();
   const langCode = useLanguageSettings();
+  const user = useUser();
+
+  const loggedUser = [
+    <NavLink to="/armoury">{languagePacks.headers?.armoury[langCode]}</NavLink>,
+    <NavLink to="/challenges-of-gods">
+      {languagePacks.headers?.challenges[langCode]}
+    </NavLink>,
+    <NavLink to="/character-view">
+      {languagePacks.headers?.character_view[langCode]}
+    </NavLink>,
+    <NavLink to="/statistics">
+      {languagePacks.headers?.statistics[langCode]}
+    </NavLink>,
+    <NavLink to="/chat">{languagePacks.headers?.chat[langCode]}</NavLink>,
+  ];
 
   return (
     <nav className="navigation">
@@ -14,28 +30,15 @@ const Navigation: FunctionComponent = () => {
           <NavLink to="/">Home</NavLink>
         </li>
         <li className="navigation__item">
-          <NavLink to="/armoury">
-            {languagePacks.headers?.armoury[langCode]}
+          <NavLink to="/about">
+            {languagePacks.headers?.about[langCode]}
           </NavLink>
         </li>
-        <li className="navigation__item">
-          <NavLink to="/challenges-of-gods">
-            {languagePacks.headers?.challenges[langCode]}
-          </NavLink>
-        </li>
-        <li className="navigation__item">
-          <NavLink to="/character-view">
-            {languagePacks.headers?.character_view[langCode]}
-          </NavLink>
-        </li>
-        <li className="navigation__item">
-          <NavLink to="/statistics">
-            {languagePacks.headers?.statistics[langCode]}
-          </NavLink>
-        </li>
-        <li className="navigation__item">
-          <NavLink to="/chat">{languagePacks.headers?.chat[langCode]}</NavLink>
-        </li>
+        {user
+          ? loggedUser.map((link) => (
+              <li className="navigation__item">{link}</li>
+            ))
+          : null}
       </ul>
     </nav>
   );
