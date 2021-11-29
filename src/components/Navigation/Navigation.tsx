7d79318ab96bcb55d79717fba1f7b8f1b1b2cc67
Przemek <przemo247@outlook.com>
2021-11-29 09:33:51 +0100

    Set nav links basing on user role

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { useLanguagePacks } from "../../hooks/useLanguagePacks";
 import { useLanguageSettings } from "../../hooks/useLanguageSettings";
@@ -8,20 +8,38 @@ const Navigation: FunctionComponent = () => {
   const languagePacks = useLanguagePacks();
   const langCode = useLanguageSettings();
   const user = useUser();
+  const [links, setLinks] = useState<JSX.Element[] | null>(null);
 
-  const loggedUser = [
-    <NavLink to="/armoury">{languagePacks.headers?.armoury[langCode]}</NavLink>,
-    <NavLink to="/challenges-of-gods">
-      {languagePacks.headers?.challenges[langCode]}
-    </NavLink>,
-    <NavLink to="/character-view">
-      {languagePacks.headers?.character_view[langCode]}
-    </NavLink>,
-    <NavLink to="/statistics">
-      {languagePacks.headers?.statistics[langCode]}
-    </NavLink>,
-    <NavLink to="/chat">{languagePacks.headers?.chat[langCode]}</NavLink>,
-  ];
+  useEffect(() => {
+    const userLinks = [
+      <NavLink to="/armoury">
+        {languagePacks.headers?.armoury[langCode]}
+      </NavLink>,
+      <NavLink to="/challenges-of-gods">
+        {languagePacks.headers?.challenges[langCode]}
+      </NavLink>,
+      <NavLink to="/character-view">
+        {languagePacks.headers?.character_view[langCode]}
+      </NavLink>,
+      <NavLink to="/statistics">
+        {languagePacks.headers?.statistics[langCode]}
+      </NavLink>,
+      <NavLink to="/chat">{languagePacks.headers?.chat[langCode]}</NavLink>,
+    ];
+
+    if (user?.role === "admin") {
+      setLinks([
+        <NavLink to="/admin-panel">
+          {languagePacks.headers?.admin_panel[langCode]}
+        </NavLink>,
+        ...userLinks,
+      ]);
+    } else if (user?.role === "player") {
+      setLinks(userLinks);
+    } else {
+      setLinks(null);
+    }
+  }, [user, langCode, languagePacks]);
 
   return (
     <nav className="navigation">
@@ -34,11 +52,11 @@ const Navigation: FunctionComponent = () => {
             {languagePacks.headers?.about[langCode]}
           </NavLink>
         </li>
-        {user
-          ? loggedUser.map((link) => (
-              <li className="navigation__item">{link}</li>
-            ))
-          : null}
+        {links?.map((link, i) => (
+          <li key={i} className="navigation__item">
+            {link}
+          </li>
+        ))}
       </ul>
     </nav>
   );
